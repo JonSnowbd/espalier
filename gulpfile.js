21bf8299ff571a97123ad9cd07d2a5209c19b4f9
Josiah Parent <vasti009@gmail.com>
2014-04-19 06:46:39 +0100

    Odds and ends. New gulp 'build' option and updated readme.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
+var gulp     = require('gulp'),
+	less     = require('gulp-less'),
+	prefix   = require('gulp-autoprefixer'),
+	minify   = require('gulp-minify-css');
 
 gulp.task('compile', function(){
 	gulp.src('./less/main.less')
 		.pipe(less())
+		.pipe(prefix())
 		.pipe(gulp.dest('./builtcss/'));
 });
 
+gulp.task('build', function(){
+	gulp.src('./less/main.less')
+		.pipe(less())
+		.pipe(prefix())
+		.pipe(minify())
+		.pipe(gulp.dest('./builtcss/min/'));
+});
+
 gulp.task('default', ['compile']);
+
 gulp.task('watch', function(){
 	gulp.watch(['./less/**/*.less', './less/*.less'], ['compile']);
 });
